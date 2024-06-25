41b6ac0e006c34117a9bd3afd4e9684c87b8e269
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-06-25 16:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (42.71%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -12,47 +12,23 @@
 class Solution {
 public:
     TreeNode* bstToGst(TreeNode* root) {
-        // Store the inorder traversal in an array.
-        vector<int> inorderTraversal;
-        inorder(root, inorderTraversal);
-
-        // Reverse the array to get descending order.
-        reverse(inorderTraversal.begin(), inorderTraversal.end());
-
-        // Modify the values in the tree.
-        replaceValues(root, inorderTraversal);
-
+        // Initialize a local variable nodeSum.
+        int nodeSum = 0;
+        bstToGstHelper(root, nodeSum);
         return root;
     }
 
 private:
-    // Perform any traversal of your choice to store node values.
-    void inorder(TreeNode* root, vector<int>& inorderTraversal) {
-        if (root == nullptr) {
+    void bstToGstHelper(TreeNode* root, int& nodeSum) {
+        // If root is null, make no changes.
+        if (!root) {
             return;
         }
-        inorder(root->left, inorderTraversal);
-        inorderTraversal.push_back(root->val);
-        inorder(root->right, inorderTraversal);
-    }
-    // Function to modify the values in the tree.
-    void replaceValues(TreeNode* root, vector<int>& inorderTraversal) {
-        if (root == nullptr) {
-            return;
-        }
-        replaceValues(root->left, inorderTraversal);
-        replaceValues(root->right, inorderTraversal);
 
-        int nodeSum = 0;
-        // Replace node with values greater than the current value.
-        for (auto i : inorderTraversal) {
-            if (i > root->val) {
-                nodeSum += i;
-            } else {
-                break;
-            }
-        }
-
-        root->val += nodeSum;
+        bstToGstHelper(root->right, nodeSum);
+        nodeSum += root->val;
+        // Update the value of root.
+        root->val = nodeSum;
+        bstToGstHelper(root->left, nodeSum);
     }
 };
