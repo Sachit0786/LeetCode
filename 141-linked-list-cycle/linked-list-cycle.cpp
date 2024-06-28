b9e9cf5b11c19864f4c32bbb98b9429b525e01a4
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-06-28 22:33:55 +0530

    Time: 12 ms (27.00%) | Memory: 10.4 MB (99.47%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+	
+		// if head is NULL then return false;
+        if(head == NULL || head->next ==NULL)
+            return false;
+        
+		// making two pointers fast and slow and assignning them to head
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+		// till fast and fast-> next not reaches NULL(fast->next != NULL is applied beacuse of nullpointer 
+        // exception, that is if fast->next==NULL then no value can be determined for fast->next->next)
+		// we will increment fast by 2 step and slow by 1 step
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+			
+			// At the point if fast and slow are at same address
+			// this means linked list has a cycle in it.
+            if(fast == slow)
+                return true;
+        }
+        
+		// if traversal reaches to NULL this means no cycle.
+        return false;
+    }
+};
