5e911bcf7beaba8bf782f69e4064ac4bc5794627
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-06-28 22:26:08 +0530

    Time: 42 ms (40.32%) | Memory: 17 MB (93.53%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -6,20 +6,50 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_map<ListNode*,int> mp;
-
-        for(ListNode* temp=headA; temp!=NULL; temp = temp->next){
-            mp[temp] = temp->val;
+        int n1=0, n2=0;
+        for(ListNode* i=headA; i!=NULL; i=i->next){
+            n1++;
+        }
+        for(ListNode* i=headB; i!=NULL; i=i->next){
+            n2++;
         }
 
-        for(ListNode* temp=headB; temp!=NULL; temp = temp->next){
-            if(mp.find(temp) != mp.end()){
-                return temp;
+        int diff=0;
+
+        if(n1>n2){
+            diff=n1-n2;
+            for(int i=0;i<diff;i++){
+                headA=headA->next;
             }
         }
-        return nullptr;
+        else{
+            diff=n2-n1;
+            for(int i=0;i<diff;i++){
+                headB=headB->next;
+            }
+        }
+
+        ListNode* temp1=headA;
+        ListNode* temp2=headB;
+        
+        while(temp1!=NULL && temp2!=NULL){
+            if(temp1==temp2){
+                return temp1;
+            }
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+        return NULL;
     }
 };
