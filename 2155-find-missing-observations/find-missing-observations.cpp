267c964d8ac6ad92a08675dc747ed000b95b0c16
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-09-06 00:41:52 +0530

    Time: 110 ms (19.36%) | Memory: 116.3 MB (86.03%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int m = rolls.size();
+        int sum = (n+m)*mean;
+
+        for(int i=0; i<m; i++){
+            sum -= rolls[i];
+        }
+
+        if(sum<0 || n*6<sum){
+            return {};
+        }
+
+        else{
+            int distributeMean = sum / n;
+            if(distributeMean==0)
+                return {};
+            int mod = sum % n; //remaining elements magnitude
+            // Distribute the remaining mod elements in nElements array.
+            vector<int> ans(n, distributeMean);
+            for (int i = 0; i < mod; i++) {
+                ans[i]++;
+                // as we are adding only 1 each time so the numbers will never get > 6
+            }
+            return ans;
+        }
+    }
+};
