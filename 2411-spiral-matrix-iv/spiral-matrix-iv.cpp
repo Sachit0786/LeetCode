9f9c268a3dfda9dc32ce5b29004cf7d141f0cb00
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-09-09 19:21:45 +0530

    Time: 163 ms (62.78%) | Memory: 130.4 MB (94.80%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
@@ -11,22 +11,24 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        // Store the east, south, west, north movements in a matrix.
-        int i = 0, j = 0, cur_d = 0,
-            movement[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        // Store the right, down, left, up movements in a matrix.
+        int i = 0, j = 0, choice = 0;
+        int movement[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         vector<vector<int>> res(m, vector<int>(n, -1));
 
         for (; head != nullptr; head = head->next) {
             res[i][j] = head->val;
-            int newi = i + movement[cur_d][0], newj = j + movement[cur_d][1];
-
+            int newi = i + movement[choice][0]; 
+            int newj = j + movement[choice][1];
+            
             // If we bump into an edge or an already filled cell, change the
             // direction.
-            if (min(newi, newj) < 0 || newi >= m || newj >= n ||
-                res[newi][newj] != -1)
-                cur_d = (cur_d + 1) % 4;
-            i += movement[cur_d][0];
-            j += movement[cur_d][1];
+            
+            if (min(newi, newj) < 0 || newi >= m || newj >= n || res[newi][newj] != -1){
+                choice = (choice + 1) % 4;
+            }
+            i += movement[choice][0];
+            j += movement[choice][1];
         }
 
         return res;
