c2ea695b3e12678e513f64ac62b96def61c9c18f
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-09-09 19:13:16 +0530

    Time: 157 ms (78.81%) | Memory: 130.5 MB (65.45%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
@@ -11,36 +11,24 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        vector<vector<int>> ans(m,vector<int>(n,-1));
-        int i = 0, j = 0;
-        int top = 0, bottom = m - 1, left = 0, right = n - 1;
-        
-        while (head != nullptr) {
-            ans[i][j] = head->val;
-            head = head->next;
+        // Store the east, south, west, north movements in a matrix.
+        int i = 0, j = 0, cur_d = 0,
+            movement[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        vector<vector<int>> res(m, vector<int>(n, -1));
 
-            // Move right
-            if (i == top && j < right) {
-                j++;
-                if (j == right) top++;
-            }
-            // Move down
-            else if (j == right && i < bottom) {
-                i++;
-                if (i == bottom) right--;
-            }
-            // Move left
-            else if (i == bottom && j > left) {
-                j--;
-                if (j == left) bottom--;
-            }
-            // Move up
-            else if (j == left && i > top) {
-                i--;
-                if (i == top) left++;
-            }
+        for (; head != nullptr; head = head->next) {
+            res[i][j] = head->val;
+            int newi = i + movement[cur_d][0], newj = j + movement[cur_d][1];
+
+            // If we bump into an edge or an already filled cell, change the
+            // direction.
+            if (min(newi, newj) < 0 || newi >= m || newj >= n ||
+                res[newi][newj] != -1)
+                cur_d = (cur_d + 1) % 4;
+            i += movement[cur_d][0];
+            j += movement[cur_d][1];
         }
 
-        return ans;
+        return res;
     }
 };
