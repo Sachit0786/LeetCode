75a53d4cec69fff007ad13058d88b5b7ce71d0ff
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2025-01-16 11:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 64.4 MB (36.18%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()%2==0 && nums2.size()%2==0)
+        // Initialize XOR results for both arrays
+        int xor1 = 0;
+        int xor2 = 0;
+
+        // Get lengths of both arrays
+        int len1 = nums1.size();
+        int len2 = nums2.size();
+
+        // If the length of both the arrays is odd then all the numbers occurs 
+        // even number of times and thus we conclude that naswer will be zero
+        if(len2 % 2 == 0 && len1 % 2 == 0)
             return 0;
-        else if(nums1.size()%2==1 && nums2.size()%2==0){
-            int ans = 0;
-            for(int& a : nums2)
-                ans ^= a;
-            return ans;
+
+        // If nums2 length is odd, each element in nums1 appears odd times in
+        // final result
+        else if (len2 % 2 != 0) {
+            for (int num : nums1) {
+                xor1 ^= num;
+            }
         }
-        else if(nums1.size()%2==0 && nums2.size()%2==1){
-            int ans = 0;
-            for(int& a : nums1)
-                ans ^= a;
-            return ans;
+
+        // If nums1 length is odd, each element in nums2 appears odd times in
+        // final result
+        if (len1 % 2 != 0) {
+            for (int num : nums2) {
+                xor2 ^= num;
+            }
         }
-        else{
-            int ans = 0;
-            for(int& a : nums1)
-                ans ^= a;
-            for(int& a : nums2)
-                ans ^= a;    
-            return ans;
-        }
+
+        // Return XOR of both results
+        return xor1 ^ xor2;
     }
 };
