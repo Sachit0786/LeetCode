9e0a9c2c4ea3253d26bf956da3dcdb620087ac5d
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2025-02-08 16:40:51 +0530

    Time: 120 ms (53.44%) | Memory: 195.9 MB (61.88%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -9,41 +9,41 @@ public:
      * If the index already exists, it removes the previous number from the mapping.
      * Then, it assigns the new number to the given index.
      *
-     * @param index The position where the number is to be stored.
-     * @param number The value to be stored at the given index.
+     * \@param index The position where the number is to be stored.
+     * \@param number The value to be stored at the given index.
      */
     void change(int index, int number) {
         // If the index is already mapped to a number, update the mappings
-        if (indexToNumbers.find(index) != indexToNumbers.end()) {
-            int previousNumber = indexToNumbers[index];
+        if (indexToValue.find(index) != indexToValue.end()) {
+            int previousValue = indexToValue[index];
 
             // Remove the index from the set of indices associated with the previous number
-            numberToIndices[previousNumber].erase(index);
+            valueToIndices[previousValue].erase(index);
 
             // If the previous number is no longer mapped to any index, remove it from the map
-            if (numberToIndices[previousNumber].empty()) {
-                numberToIndices.erase(previousNumber);
+            if (valueToIndices[previousValue].empty()) {
+                valueToIndices.erase(previousValue);
             }
         }
 
-        // Update the index-to-number mapping
-        indexToNumbers[index] = number;
+        // Update the index-to-value mapping
+        indexToValue[index] = number;
 
         // Insert the index into the set associated with the new number
-        numberToIndices[number].insert(index);
+        valueToIndices[number].insert(index);
     }
 
     /**
      * Finds and returns the smallest index associated with a given number.
      *
-     * @param number The number to search for.
-     * @return The smallest index containing the number, or -1 if not found.
+     * \@param number The number to search for.
+     * \@return The smallest index containing the number, or -1 if not found.
      */
     int find(int number) {
         // Check if the number exists in the mapping
-        if (numberToIndices.find(number) != numberToIndices.end()) {
+        if (valueToIndices.find(number) != valueToIndices.end()) {
             // Since the indices are stored in a sorted set, return the smallest index
-            return *numberToIndices[number].begin();
+            return *valueToIndices[number].begin();
         }
         // Return -1 if the number is not present
         return -1;
@@ -51,10 +51,10 @@ public:
 
 private:
     // Stores the mapping of each number to a sorted set of indices where it appears.
-    unordered_map<int, set<int>> numberToIndices;
+    unordered_map<int, std::set<int>> valueToIndices;
 
     // Stores the mapping of each index to its associated number.
-    unordered_map<int, int> indexToNumbers;
+    unordered_map<int, int> indexToValue;
 };
 
 /**
